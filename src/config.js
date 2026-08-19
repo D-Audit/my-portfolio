e9aa0380f2ee89754b6427747190acd097167272
Kayiranga <donjesuskayiranga@gmail.com>
2026-08-19 10:00:00 -0400

    Update portfolio files (38/60)

--- src/config.js
+++ src/config.js
@@ -1,26 +1,19 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'donjesuskayiranga@gmail.com',
+  phone: '+250795822174',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/D-Audit',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://www.instagram.com/donje_sus12/',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://www.linkedin.com/in/don-kayiranga-2b9177408/',
     },
   ],
 
@@ -44,7 +37,7 @@ module.exports = {
   ],
 
   colors: {
-    green: '#64ffda',
+    green: '#38e1b4',
     navy: '#0a192f',
     darkNavy: '#020c1b',
   },
